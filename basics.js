1f066e08627687d2556ba54860b9326c33de2632
reddyarchana841-code <reddyarchana841@gmail.com>
2026-03-11 15:01:56 +0530

    js.1

---
+++ basics.js
@@ -0,0 +1,21 @@
+var name = "Rahul";        
+var age = 22;             
+var isStudent = true;      
+let city = "Chittoor";     
+let marks = [85, 90, 95];  
+let details = {            
+  id: 1,
+  course: "JavaScript"
+};
+const country = "India";  
+const pi = 3.14;           
+const isActive = false;   
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("City:", city);
+console.log("Marks:", marks);
+console.log("Details:", details);
+console.log("Country:", country);
+console.log("Pi:", pi);
+console.log("Is Active:", isActive);
